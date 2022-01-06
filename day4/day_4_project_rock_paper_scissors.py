3b609de7715cab4671e6e9d9d5ed5241941cf68d
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-01-05 21:08:30 -0300

    day 4 - rock paper scissors - improved

--- day4/day_4_project_rock_paper_scissors.py
+++ day4/day_4_project_rock_paper_scissors.py
@@ -55,27 +55,23 @@ tied = '''
 
 hand_signs = [rock, paper, scissors]
 
-user_choise = int(input('\n\nWhat do you choose? Type 0 for Rock, 1 for Paper '
-                        'or 2 for Scissors.\n'))
+user_choise = int(input('\n\nWhat do you choose? Type 0 for Rock, 1 for '
+                        'Paper or 2 for Scissors.\n'))
 computer_choise = random.randint(0, 2)
 
-if user_choise == computer_choise:
-    print(f'You chose:{hand_signs[user_choise]}\nComputer chose:'
-          f'{hand_signs[computer_choise]}\n\nResult:{tied}'
-          )
-elif user_choise == 0 and computer_choise == 1:
-    print(f'You chose:{hand_signs[user_choise]}\nComputer chose:'
-          f'{hand_signs[computer_choise]}\n\nResult:{lost}'
-          )
-elif user_choise == 1 and computer_choise == 2:
-    print(f'You chose:{hand_signs[user_choise]}\nComputer chose:'
-          f'{hand_signs[computer_choise]}\n\nResult:{lost}'
-          )
-elif user_choise == 2 and computer_choise == 0:
-    print(f'You chose:{hand_signs[user_choise]}\nComputer chose:'
-          f'{hand_signs[computer_choise]}\n\nResult:{lost}'
-          )
+if (user_choise >= 3) or (user_choise < 0):
+    print(f'You chose an invalid number, so:{lost}')
 else:
-    print(f'You chose:{hand_signs[user_choise]}\nComputer chose:'
-          f'{hand_signs[computer_choise]}\n\nResult:{won}'
-          )
+    result = f'\n\nYou chose:{hand_signs[user_choise]}\nComputer chose:'\
+             f'{hand_signs[computer_choise]}\n\nResult:'
+
+    if user_choise == computer_choise:
+        print(result + tied)
+    elif user_choise == 0 and computer_choise == 1:
+        print(result + lost)
+    elif user_choise == 1 and computer_choise == 2:
+        print(result + lost)
+    elif user_choise == 2 and computer_choise == 0:
+        print(result + lost)
+    else:
+        print(result + won)
