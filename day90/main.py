7a6875c39df75ead56558493b4c2a9939ca2808e
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-04-01 22:24:38 -0300

    day 90 - text to speech - completed

--- day90/main.py
+++ day90/main.py
@@ -1,42 +1,60 @@
 from base64 import b64decode
 from local_settings.local_settings import API_GOOGLE_CLOUD_TEXT_TO_SPEECH
 import requests
+import fitz
 
 GCTS_ENDPOINT = "https://texttospeech.googleapis.com/v1beta1/text:synthesize"
+pdf_dir = "day90\\assets\\the_black_cat.pdf"
 
 
-google_parameters = {
-    "audioConfig": {
-        "audioEncoding": "LINEAR16",
-        "pitch": 0,
-        "speakingRate": 1
-    },
-    "input": {
-        "text": "Google Cloud Text-to-Speech enables developers to synthesize natural-sounding speech with 100+ voices, available in multiple languages and variants. It applies DeepMind’s groundbreaking research in WaveNet and Google’s powerful neural networks to deliver the highest fidelity possible. As an easy-to-use API, you can create lifelike interactions with your users, across many applications and devices."
-    },
-    "voice": {
-        "languageCode": "en-US",
-        "name": "en-US-Wavenet-D",
-    },
-    # "X-Goog-Api-Key": API_GOOGLE_CLOUD_TEXT_TO_SPEECH,
-
-}
 headers = {
     "X-Goog-Api-Key": API_GOOGLE_CLOUD_TEXT_TO_SPEECH,
 }
 
-response = requests.post(
-    url=GCTS_ENDPOINT,
-    json=google_parameters,
-    headers=headers,
-)
-response.raise_for_status()
-texto = response.json()
-texto_string = texto['audioContent']
 
+with fitz.open(pdf_dir) as doc:
+    for part in str(doc.name).split('\\'):
+        if ".pdf" in part:
+            nome_arquivo = part.split('.')[0]
 
-with open('text.mp3', mode='wb') as file:
-    audio = b64decode(texto_string)
-    file.write(audio)
+    text = ""
+    num_pagina = -1
+    for page in doc:
+        num_pagina += 1
+        text = page.get_text()
+    # print(text)
 
-print(texto)
+        google_parameters = {
+            "audioConfig": {
+                "audioEncoding": "LINEAR16",
+                "pitch": 0,
+                "speakingRate": 1
+            },
+            "input": {
+                "text": text,
+            },
+            "voice": {
+                "languageCode": "en-US",
+                "name": "en-US-Wavenet-D",
+            },
+        }
+
+        response = requests.post(
+            url=GCTS_ENDPOINT,
+            json=google_parameters,
+            headers=headers,
+        )
+
+        response.raise_for_status()
+        texto = response.json()
+        texto_string = texto['audioContent']
+
+        assets_dir = "day90\\assets"
+        file_name = f"{nome_arquivo}\\{nome_arquivo}_{num_pagina}.mp3"
+        mp3_dir = f"{assets_dir}\\{file_name}"
+
+        with open(mp3_dir, mode='wb') as file:
+            audio = b64decode(texto_string)
+            file.write(audio)
+
+        print(texto)
