93f40330ed7822f5c6ebdd606a637e41dd880c21
Filipe Henrique Leite dos Reis <filipe.henrique.reis@gmail.com>
2022-04-03 18:39:03 -0300

    day 93 - automate the google dinosaur game

--- day93/main.py
+++ day93/main.py
@@ -1,6 +1,26 @@
-
-from time import sleep
 import pyautogui
+from time import sleep
 
+sleep(2)
+pyautogui.press('space')
 
+i = 0
+while True:
+    screen = pyautogui.screenshot()
 
+    tree_low1 = screen.getpixel((375, 480))
+    tree_low2 = screen.getpixel((400, 480))
+
+    tree_high1 = screen.getpixel((375, 440))
+    tree_high2 = screen.getpixel((400, 440))
+
+    if tree_low1[0] != 255 or tree_low2[0] != 255 or \
+            tree_high1[0] != 255 or tree_high2[0] != 255:
+        # save screenshot to analyse
+        i += 1
+        screen.save(f"day93\\{i}.jpg")
+
+        # press the 'space' to jump
+        pyautogui.press('space')
+        print('Espaço pressionado.')
+        sleep(0.0001)
